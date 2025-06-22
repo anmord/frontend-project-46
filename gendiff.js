492acf70d20d2f93716d3d66b2a22b4c4d650bed
Антон Таракин <anton_tarakin@mail.ru>
2025-06-22 18:52:38 +0300

    Step 7
    Add search for differences in deep files and tests for them

--- gendiff.js
+++ gendiff.js
@@ -4,14 +4,18 @@ import fileTypeDefinition from './src/parsers.js'
 const program = new Command()
 
 const command = (filepath1, filepath2) => {
-  fileTypeDefinition(filepath1, filepath2)
+  const options = program.opts()
+  const formatter = options.format || 'stylish'
+  if (formatter === 'stylish') {
+    fileTypeDefinition(filepath1, filepath2)
+  }
 }
 
 program
   .name('gendiff')
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action(command)
 
