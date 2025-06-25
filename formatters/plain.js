ed4d1cdafb4b2ee98ec26737557338388c53e654
Антон Таракин <anton_tarakin@mail.ru>
2025-06-25 21:32:51 +0300

    Step 8
    Add plain format and tests

---
+++ formatters/plain.js
@@ -0,0 +1,42 @@
+import _ from 'lodash'
+
+const plainFormatter = (filepath1, filepath2) => {
+  const getFinalObj = compareFile(filepath1, filepath2)
+  console.log(getFinalObj)
+  return getFinalObj
+}
+
+const stringify = (value) => {
+  if (_.isObject(value)) {
+    return '[complex value]'
+  }
+  if (typeof value === 'string') {
+    return `'${value}'`
+  }
+  return `${value}`
+}
+
+const compareFile = (file1, file2, path = '') => {
+  const keys = _.union(Object.keys(file1), Object.keys(file2)).sort()
+
+  const result = keys.flatMap((key) => {
+    const fullPath = path ? `${path}.${key}` : key
+
+    if (!_.has(file2, key)) {
+      return `Property '${fullPath}' was removed`
+    }
+    if (!_.has(file1, key)) {
+      return `Property '${fullPath}' was added with value: ${stringify(file2[key])}`
+    }
+    if (_.isObject(file1[key]) && _.isObject(file2[key])) {
+      return compareFile(file1[key], file2[key], fullPath)
+    }
+    if (file1[key] !== file2[key]) {
+      return `Property '${fullPath}' was updated. From ${stringify(file1[key])} to ${stringify(file2[key])}`
+    }
+    return []
+  })
+  return result.join('\n')
+}
+
+export default plainFormatter
--- src/parsers.js
+++ src/parsers.js
@@ -1,9 +1,9 @@
 import fs from 'fs'
 import path from 'path'
 import yaml from 'js-yaml'
-import parsing from './parse.js'
+import selectFormatters from '../formatters/index.js'
 
-const fileTypeDefinition = (filepath1, filepath2) => {
+const fileTypeDefinition = (filepath1, filepath2, formatter) => {
   const extname = path.extname(filepath1)
   const absolutePath1 = path.resolve(process.cwd(), filepath1)
   const absolutePath2 = path.resolve(process.cwd(), filepath2)
@@ -13,9 +13,9 @@ const fileTypeDefinition = (filepath1, filepath2) => {
   switch (extname) {
     case '.yaml':
     case '.yml':
-      return parsing(yaml.load(fileContent1), yaml.load(fileContent2))
+      return selectFormatters(yaml.load(fileContent1), yaml.load(fileContent2), formatter)
     case '.json':
-      return parsing(JSON.parse(fileContent1), JSON.parse(fileContent2))
+      return selectFormatters(JSON.parse(fileContent1), JSON.parse(fileContent2), formatter)
     default:
       console.log(`Unsupported file extension: ${extname}`)
       return undefined
