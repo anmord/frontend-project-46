8dd41b1e811f5b3b3bf41b61444e22ef134345eb
Антон Таракин <anton_tarakin@mail.ru>
2025-07-01 22:27:27 +0300

    fix ci

--- __tests__/parse.test.js
+++ __tests__/parse.test.js
@@ -211,7 +211,7 @@ test('Correct_json_.json_1', () => {
   const filePath1 = path.resolve('__fixtures__', 'file_json_1_test.json')
   const filePath2 = path.resolve('__fixtures__', 'file_json_2_test.json')
 
-  const expected = `[{"key":"common","type":"nested","children":[{"key":"follow","type":"added","value":false},
+  const expected = [{"key":"common","type":"nested","children":[{"key":"follow","type":"added","value":false},
   {"key":"setting1","type":"unchanged","value":"Value 1"},{"key":"setting2","type":"removed","value":200},
   {"key":"setting3","type":"updated","oldValue":true,"newValue":null},{"key":"setting4","type":"added","value":"blah blah"},
   {"key":"setting5","type":"added","value":{"key5":"value5"}},
@@ -219,17 +219,17 @@ test('Correct_json_.json_1', () => {
   {"key":"key","type":"unchanged","value":"value"},{"key":"ops","type":"added","value":"vops"}]}]},
   {"key":"group1","type":"nested","children":[{"key":"baz","type":"updated","oldValue":"bas","newValue":"bars"},{"key":"foo","type":"unchanged","value":"bar"},
   {"key":"nest","type":"updated","oldValue":{"key":"value"},"newValue":"str"}]},{"key":"group2","type":"removed","value":{"abc":12345,"deep":{"id":45}}},
-  {"key":"group3","type":"added","value":{"deep":{"id":{"number":45}},"fee":100500}}]`
-  expect(fileTypeDefinition(filePath1, filePath2, 'json')).toEqual(JSON.parse(expected))
+  {"key":"group3","type":"added","value":{"deep":{"id":{"number":45}},"fee":100500}}]
+  expect(JSON.parse(fileTypeDefinition(filePath1, filePath2, 'json'))).toEqual(expected)
 })
 
 test('Correct_json_.json_2', () => {
   const filePath1 = path.resolve('__fixtures__', 'file_json_3_test.json')
   const filePath2 = path.resolve('__fixtures__', 'file_json_4_test.json')
 
-  const expected = `[{"key": "common", "type": "added", "value": {"follow": false, "setting1": "Value 1", "setting3": null, 
+  const expected = [{"key": "common", "type": "added", "value": {"follow": false, "setting1": "Value 1", "setting3": null, 
   "setting4": "blah blah", "setting5": {"key5": "value5"}, "setting6": {"doge": {"wow": "so much"}, "key": "value", "ops": "vops"}}}, 
   {"key": "group1", "type": "added", "value": {"baz": "bars", "foo": "bar", "nest": "str"}}, {"key": "group3", "type": "added", 
-  "value": {"deep": {"id": {"number": 45}}, "fee": 100500}}]`
-  expect(fileTypeDefinition(filePath1, filePath2, 'json')).toEqual(JSON.parse(expected))
+  "value": {"deep": {"id": {"number": 45}}, "fee": 100500}}]
+  expect(JSON.parse(fileTypeDefinition(filePath1, filePath2, 'json'))).toEqual(expected)
 })
